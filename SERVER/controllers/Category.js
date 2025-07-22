f2b8b36d1616a44c4ede400673c071bfcc793e9c
Md Tauseef <tauseef426558@gmail.com>
2025-07-22 16:22:17 +0530

    Added categoryPageDetails in Category.js

--- SERVER/controllers/Category.js
+++ SERVER/controllers/Category.js
@@ -50,3 +50,53 @@ exports.showAllCategories = async (req, res) => {
         });
     }
 }
+
+//category page details
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        //get categoryId
+        const {categoryId} = req.body;
+
+        //get courses for specified categoryId
+        const selectedCategory = await Category.findById(categoryId)
+                                            .populate("courses")
+                                            .exec();
+                        
+        //validation
+        if(!selectedCategory){
+            return res.status(404).json({
+                success: true,
+                message: 'Data not found'
+            });
+        }
+
+        //get course for different categories
+        const differentCategories = await Category.find({ _id: {$ne: categoryId}})
+                                            .populate("courses")
+                                            .exec();
+
+        //get top selling courses
+        const allCategories = await Category.find().populate("course");
+        const allCourses = allCategories.flatMap((category) => category.course);
+        const mostSellingCourses = allCourses
+            .sort((a,b) => b.sold-a.sold)
+            .slice(0, 10);
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: '',
+            data: {
+                selectedCategory,
+                differentCategories,
+                mostSellingCourses,
+            },
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
